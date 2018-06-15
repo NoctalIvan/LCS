f9b247dcb72c8ee5604b4596f85a8df139b0d603
NoctalIvan <sedletzk@gmail.com>
2018-06-16 00:08:53 +0200

    first assets, base gameloop

--- js/main.js
+++ js/main.js
@@ -7,20 +7,48 @@ window.addEventListener("resize", function () {
 });
 
 // Create some text. Not important for fullscreen
-function setupDemo() {
+function setup() {
   app = new PIXI.Application(window.innerWidth, window.innerHeight, {
-    // backgroundColor: 0xcccccc
-    // transparent: true
+    backgroundColor: 0xcccccc,
+    transparent: true,
   });
   document.body.appendChild(app.view);
 
-  
-  // Update the text every pixi frame or 'tick'
-  app.ticker.add(loop)
+  const getImagePath = (a) => 'js/assets/' + a + ".png"
+  let images = ["logo", "health"]
+
+  let data = {
+    life: 100,
+  }
+
+  let loader = PIXI.loader.add(images.map(getImagePath)).load(() => {
+
+    // textures
+    let textures = {}
+    images.forEach(i => textures[i] = PIXI.loader.resources[getImagePath(i)].texture)
+
+    let sprites = {
+      logo: new PIXI.Sprite(textures.logo),
+    }
+
+    // position
+    sprites.logo.x = innerWidth/2 - sprites.logo.width /2
+    sprites.logo.y = innerHeight/2 - sprites.logo.height /2
+    console.log(sprites.logo.x, sprites.logo.width)
+
+    sp
+
+    // add sprites
+    for(var s in sprites) {
+      app.stage.addChild(sprites[s])
+    }
+
+    app.ticker.add(loop)
+  })
 }
 
-const loop = () => {
-  console.log("a")
+const loop = (delta) => {
+
 }
 
 setup();
